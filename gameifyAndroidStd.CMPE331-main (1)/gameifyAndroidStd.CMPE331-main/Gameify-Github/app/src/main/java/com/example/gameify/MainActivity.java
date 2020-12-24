9bbce04abd2360b275efbb9a2df39d088adbff47
Mustafa Bahadir Aykol <bahadir.aykol@bilgiedu.net>
2020-12-24 23:35:02 +0300

    Project

---
+++ gameifyAndroidStd.CMPE331-main (1)/gameifyAndroidStd.CMPE331-main/Gameify-Github/app/src/main/java/com/example/gameify/MainActivity.java
@@ -0,0 +1,152 @@
+package com.example.gameify;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tv_gameify, tv_username, tv_user_password, tv_create_account;
+    private EditText et_username, et_password;
+    private CheckBox cb_robot;
+    Button but_login_button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv_gameify = (TextView) findViewById(R.id.tv_gameify);
+        tv_username = (TextView) findViewById(R.id.tv_username);
+        tv_user_password = (TextView) findViewById(R.id.tv_user_password);
+        tv_create_account = (TextView) findViewById(R.id.tv_create_account);
+        et_username = (EditText) findViewById(R.id.et_username);
+        et_password = (EditText) findViewById(R.id.et_password);
+        cb_robot = (CheckBox) findViewById(R.id.cb_robot);
+        but_login_button = (Button) findViewById(R.id.but_login_button);
+
+
+        loadData();
+
+
+        // Silinecek
+        tv_username.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadData();
+                Intent intent =  new Intent(MainActivity.this, afterLoginMainPage.class);
+                int index = 0;
+                intent.putExtra("index",index);
+                startActivity(intent);
+
+            }
+        });
+
+        tv_create_account.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, createAccountWindow.class);
+                startActivity(intent);
+            }
+        });
+
+        but_login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadData();
+                int index = 0;
+                String username_from_et = et_username.getText().toString();
+                String password_from_et = et_password.getText().toString();
+
+                if (userAccount.getUserAccountArrayList().size() == 0){
+                    Toast.makeText(MainActivity.this, "There is no entered data!!", Toast.LENGTH_SHORT).show();
+                }else {
+                    for (int i = 0; i < userAccount.getUserAccountArrayList().size(); i++) {
+                        if (username_from_et.equalsIgnoreCase(userAccount.getUserAccountArrayList().get(i).getUsername())) {
+                            if (password_from_et.equalsIgnoreCase(userAccount.getUserAccountArrayList().get(i).getPassword())) {
+                                index = i;
+                                if (cb_robot.isChecked()) {
+                                    et_username.setText("");
+                                    et_password.setText("");
+                                    Toast.makeText(MainActivity.this, "Login Succesful!!", Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(MainActivity.this, afterLoginMainPage.class);
+                                    intent.putExtra("index", index);
+                                    startActivity(intent);
+                                } else {
+                                    Toast.makeText(MainActivity.this, "Please verify you're human!!", Toast.LENGTH_SHORT).show();
+                                }
+                            } else {
+                                Toast.makeText(MainActivity.this, "Entered password is not matched with username's password!!", Toast.LENGTH_SHORT).show();
+                                et_password.setText("");
+                            }
+                        }
+                    }
+                }
+            }
+        });
+    }
+
+    private void firstMethodofAdmin() {
+        userAccount userAdmin = new userAccount("Admin","Admin","Admin","Admin","admin","123","123");
+        userAccount.getUserAccountArrayList().add(userAdmin);
+        gameData gd = new gameData("admin", "admin", "100", "100", "100", "100");
+        gameData.getAllUserData().add(gd);
+        saveData();
+    }
+
+    private void saveData(){
+        SharedPreferences sharedPreferences = getSharedPreferences("shared_preferences", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(userAccount.getUserAccountArrayList());
+        editor.putString("userlist",json);
+        String jsonAllUserData = gson.toJson(gameData.getAllUserData());
+        editor.putString("jsonAllUserData", jsonAllUserData);
+        editor.apply();
+
+    }
+
+
+    private void clearData(){
+        SharedPreferences sharedPreferences = getSharedPreferences("shared_preferences", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.clear();
+        /*
+        editor.remove("userlist");
+        editor.remove("jsonAllUserData");
+         */
+        editor.apply();
+
+    }
+
+    private void loadData(){
+        SharedPreferences sharedPreferences = getSharedPreferences("shared_preferences", MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = sharedPreferences.getString("userlist",null);
+        Type type = new TypeToken<ArrayList<userAccount>>() {}.getType();
+        userAccount.setUserAccountArrayList(gson.fromJson(json, type));
+        String jsonAllUserData = sharedPreferences.getString("jsonAllUserData", null);
+        Type typeSpecial = new TypeToken<ArrayList<gameData>>() {
+        }.getType();
+        gameData.setAllUserData(gson.fromJson(jsonAllUserData, typeSpecial));
+    }
+
+
+
+
+}
---
+++ gameifyAndroidStd.CMPE331-main (1)/gameifyAndroidStd.CMPE331-main/Gameify-Github/app/src/main/java/com/example/gameify/createAccountWindow.java
@@ -0,0 +1,196 @@
+package com.example.gameify;
+
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.gson.Gson;
+
+public class createAccountWindow extends AppCompatActivity {
+
+    private EditText et_name_accPage, et_surname_accPage;
+    private EditText et_email_accPage;
+    private EditText et_username_accPage, et_password_accPage, et_re_password_accPage;
+    Button but_create_button_accPage;
+    Spinner sp_age_accPage;
+    private String[] age_data_array = new String[66];
+    private String age_output_fromSpinner = "";
+    private String game_output_fromSpinner = "";
+    private String level_output_fromSpinner = "";
+
+    private Spinner sp_game, sp_level;
+    String[] games = {"Games","CS-GO", "PUBG", "GTA V", "LOL" , "Metin 2", "Rocket League", "Teamfight Tactics",
+            "Dota 2","Smite","WOW", "TERA", "FIFA 21", "Dota Auto Chess" ,"Tom Clancy's Rainbow Six Siege","Valorant"};
+    private String[] level_data = new String[102];
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account_window);
+
+        int age = 10;
+        age_data_array[0] = "Age";
+        for (int i=1 ; i<=65 ; i++){
+            age_data_array[i] = String.valueOf(age);
+            age++;
+        }
+
+        int level = 0;
+        level_data[0] = "Level";
+        for (int i=1 ; i<level_data.length ;i++){
+            level_data[i] = String.valueOf(level);
+            level++;
+        }
+
+        sp_age_accPage = (Spinner) findViewById(R.id.sp_age_accPage);
+        et_name_accPage = (EditText) findViewById(R.id.et_name_accPage);
+        et_surname_accPage = (EditText) findViewById(R.id.et_surname_accPage);
+        et_email_accPage = (EditText) findViewById(R.id.et_email_accPage);
+        et_username_accPage = (EditText) findViewById(R.id.et_username_accPage);
+        et_password_accPage = (EditText) findViewById(R.id.et_password_accPage);
+        et_re_password_accPage = (EditText) findViewById(R.id.et_re_password_accPage);
+        but_create_button_accPage = (Button) findViewById(R.id.but_create_button_accPage);
+        sp_game = (Spinner) findViewById(R.id.sp_game);
+        sp_level= (Spinner) findViewById(R.id.sp_level);
+
+        ArrayAdapter aa_age = new ArrayAdapter(this, android.R.layout.simple_spinner_item, age_data_array);
+        aa_age.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sp_age_accPage.setAdapter(aa_age);
+
+        ArrayAdapter aa_game = new ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, games);
+        aa_game.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sp_game.setAdapter(aa_game);
+
+        ArrayAdapter aa_level = new ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, level_data);
+        aa_level.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sp_level.setAdapter(aa_level);
+
+
+        sp_age_accPage.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                TextView tv = (TextView) view;
+                if (position == 0){
+                    tv.setTextColor(Color.GRAY);
+                    age_output_fromSpinner = "";
+                }else{
+                    tv.setTextColor(Color.BLACK);
+                    age_output_fromSpinner = parent.getItemAtPosition(position).toString();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        sp_game.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                TextView tv = (TextView) view;
+                if (position == 0){
+                    tv.setTextColor(Color.GRAY);
+                    game_output_fromSpinner = "";
+                }else{
+                    tv.setTextColor(Color.BLACK);
+                    game_output_fromSpinner = parent.getItemAtPosition(position).toString();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        sp_level.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                TextView tv = (TextView) view;
+                if (position == 0){
+                    tv.setTextColor(Color.GRAY);
+                    level_output_fromSpinner = "";
+                }else{
+                    tv.setTextColor(Color.BLACK);
+                    level_output_fromSpinner = parent.getItemAtPosition(position).toString();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        but_create_button_accPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                String name = et_name_accPage.getText().toString().trim();
+                String surname = et_surname_accPage.getText().toString().trim();
+                String age = age_output_fromSpinner;
+                String email = et_email_accPage.getText().toString().trim();
+                String username = et_username_accPage.getText().toString().trim();
+                String password = et_password_accPage.getText().toString().trim();
+                String re_password = et_re_password_accPage.getText().toString().trim();
+                String game = game_output_fromSpinner;
+                String level = level_output_fromSpinner;
+
+
+                boolean status = check(name,surname,age,email,username,password,re_password);
+
+                if (status){
+                    if (password.equalsIgnoreCase(re_password)){
+
+
+
+                        userAccount new_user = new userAccount(name, surname, age, email, username, password, re_password);
+                        userAccount.getUserAccountArrayList().add(new_user);
+                        gameData temp = new gameData(username, age, "0", "0", "0", "0");
+                       //gameData.getAllUserData().add(temp);
+
+
+                        DataBaseHelper dataBaseHelper = new DataBaseHelper(createAccountWindow.this);
+                        dataBaseHelper.addData(new_user, temp);
+
+
+                        createAccountWindow.this.finish();
+                    }else{
+                        Toast.makeText(createAccountWindow.this, "Please check your passwords..", Toast.LENGTH_SHORT).show();
+                        et_password_accPage.setText("");
+                        et_re_password_accPage.setText("");
+                    }
+                }else {
+                    Toast.makeText(createAccountWindow.this, "Please fill all information!!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+    private boolean check(String name, String surname, String age, String email, String username, String password, String re_password) {
+        return !name.isEmpty() && !surname.isEmpty() && !age.isEmpty() && !email.isEmpty() && !username.isEmpty() && !password.isEmpty() && !re_password.isEmpty();
+    }
+
+    private void saveData(){
+        SharedPreferences sharedPreferences = getSharedPreferences("shared_preferences", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(userAccount.getUserAccountArrayList());
+        editor.putString("userlist",json);
+        String jsonAllUserData = gson.toJson(gameData.getAllUserData());
+        editor.putString("jsonAllUserData", jsonAllUserData);
+        editor.apply();
+    }
+}
